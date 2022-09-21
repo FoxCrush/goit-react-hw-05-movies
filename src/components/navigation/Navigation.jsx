05f626b8066d65422198ccd82451ee92a898b734
FoxCrush <63466872+FoxCrush@users.noreply.github.com>
2022-09-21 16:15:13 +0300

    css for navigation

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './navigation.module.css';
 
 const Navigation = () => {
   return (
-    <div className="navigationContainer">
-      <ul className="navList">
-        <li className="navListItem">
-          <Link to={'/'}>Home</Link>
+    <div className={styles.navigationContainer}>
+      <ul className={styles.navList}>
+        <li className={styles.navListItem}>
+          <Link to={'/'}>
+            <span className={styles.navListItemTitle}>HOME</span>
+          </Link>
         </li>
-        <li className="navListItem">
-          <Link to={'/movies'}>Movies</Link>
+        <li className={styles.navListItem}>
+          <Link to={'/movies'}>
+            <span className={styles.navListItemTitle}>MOVIES</span>
+          </Link>
         </li>
       </ul>
     </div>
